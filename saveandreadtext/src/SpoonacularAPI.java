bf8af19c9a3fb229ed74d73043ab98103bdbc8a1
Jonah <jonah_nash24@milton.edu>
2024-01-25 23:19:22 -0500

    food api

--- saveandreadtext/src/SpoonacularAPI.java
+++ saveandreadtext/src/SpoonacularAPI.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -35,14 +37,17 @@ public class SpoonacularAPI {
     public int likes;
 
     public JFrame f;
-    public JTextField input;
-    public JTextField input2;
-    public JTextField output;
-    public JPanel centerPanel;
-    public JPanel rightPanel;
-    public JPanel inputPanel;
-    public JButton enter, zero, one, two, three, four, five, six, seven, eight, nine;
+    public JTextField input, input2;
+    public JTextField output, output2, output3;
+    public JPanel centerPanel, rightPanel, inputPanel;
+    public JButton enter, enter2, zero, one, two, three, four, five, six, seven, eight, nine;
     public static String[] meals = new String[10];
+    static String moodInput = "";
+    static String ingredientInput = "banana";
+    static int chosenRecipe = 1;
+    static String moodOutput = "";
+    static String mealsOutput = "";
+    static String descriptionOutput = "";
     public static void main(String[] args) throws ParseException {
         SpoonacularAPI myMain;
         myMain = new SpoonacularAPI();
@@ -64,7 +69,10 @@ public class SpoonacularAPI {
         input = new JTextField();
         input2 = new JTextField();
         output= new JTextField();
-        enter = new JButton("ENTER");
+        output2 = new JTextField();
+        output3 = new JTextField();
+        enter = new JButton("ENTER1");
+        enter2 = new JButton("ENTER2");
         zero = new JButton("0");
         one = new JButton("1");
         two = new JButton("2");
@@ -76,17 +84,154 @@ public class SpoonacularAPI {
         eight = new JButton("8");
         nine = new JButton("9");
 
-        //enter.addActionListener(new ActionListener);
+        enter.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                moodInput = input.getText();
+                try {
+                    pullMood();
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
+                output.setText(moodOutput);
+            }
+        });
+        enter2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ingredientInput = input2.getText();
+                try {
+                    pullSpoonacular();
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
+                output2.setText(mealsOutput);
+            }
+        });
+        zero.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 0;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        one.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 1;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        two.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 2;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        three.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 3;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        four.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 4;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        five.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 5;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        six.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 6;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        seven.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 7;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        eight.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 8;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+        nine.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                chosenRecipe = 9;
+                try{
+                    pullDescription();
+                }catch(ParseException ex){
+                    throw new RuntimeException(ex);
+                }
+                output3.setText(descriptionOutput);
+            }
+        });
+
+
         inputPanel.setLayout(new GridLayout(2,1));
         f.add(rightPanel,BorderLayout.EAST);
         f.add(inputPanel,BorderLayout.NORTH);
+        f.add(centerPanel,BorderLayout.CENTER);
         inputPanel.add(input);
         inputPanel.add(input2);
-        f.add(output,BorderLayout.CENTER);
-        centerPanel.setSize(400,400);
+        centerPanel.setLayout(new GridLayout(3,1));
+        centerPanel.add(output);
+        centerPanel.add(output2);
+        centerPanel.add(output3);
 //        f.add(centerPanel,BorderLayout.CENTER);
-        rightPanel.setLayout(new GridLayout(11,1));
+        rightPanel.setLayout(new GridLayout(12,1));
         rightPanel.add(enter);
+        rightPanel.add(enter2);
         rightPanel.add(zero);
         rightPanel.add(one);
         rightPanel.add(two);
@@ -101,11 +246,11 @@ public class SpoonacularAPI {
 
     }
     public static void pullMood() throws ParseException{
-        String apiKey = "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";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your mood");
-        String moodInput = scanner.next();
-        String prompt = "10 ingredients for when in a " + moodInput + " mood (no description)";
+        String apiKey = "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";
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter your mood");
+        //String moodInput = scanner.next();
+        String prompt = "10 edible ingredients for when in a " + moodInput + " mood (no description)";
 
         try {
 
@@ -176,6 +321,8 @@ public class SpoonacularAPI {
             JSONObject message = (JSONObject) ch.get("message");
             String content = (String) message.get("content");
             System.out.println(content);
+            moodOutput = content;
+
         }
         catch (Exception e){
             e.printStackTrace();
@@ -188,9 +335,9 @@ public class SpoonacularAPI {
         String apiUrl = "https://api.spoonacular.com/recipes/findByIngredients";
         String totalJson = "";
         String output = "";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your ingredients");
-        String ingredientInput = scanner.next();
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter your ingredients");
+        //String ingredientInput = scanner.next();
         // Request parameters (customize based on your needs)
         String ingredients = ingredientInput;
         int number = 10;
@@ -244,23 +391,27 @@ public class SpoonacularAPI {
                 String title = (String) jsonObject.get("title");
                 System.out.println(n + ": " + title);
                 meals[n] = (String)title;
+                if(n<number-1) {
+                    mealsOutput = mealsOutput + "" + n + ") " + meals[n] + ", ";
+                }else{
+                    mealsOutput = mealsOutput + "" + n + ") " +  meals[n] + ".";
+                }
                 long usedIngredients = (long) jsonObject.get("usedIngredientCount");
 //            int usedIngredients= (int) usedIngredientsLong;
 //            System.out.println("ingredients used: " + usedIngredients);
             }
-
         }
         catch (Exception e){
             e.printStackTrace();
         }
     }
     public static void pullDescription() throws ParseException{
-        String apiKey = "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";
+        String apiKey = "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";
         String output = "";
         String totalJson = "";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your which recipe would you like to learn more about?");
-        int chosenRecipe = scanner.nextInt();
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter your which recipe would you like to learn more about?");
+        //int chosenRecipe = scanner.nextInt();
 //        int cRInt = (chosenRecipe);
         System.out.println(meals[chosenRecipe]);
         String prompt = "Describe " + meals[chosenRecipe] + " in two sentences. The first sentence should include flavor facts and the second should include ingredients and nutrition facts.";
@@ -324,17 +475,18 @@ public class SpoonacularAPI {
             JSONObject message = (JSONObject) ch.get("message");
             String content = (String) message.get("content");
             System.out.println(content);
+            descriptionOutput = content;
         }
         catch (Exception e){
             e.printStackTrace();
         }
         System.out.println("Do you like THIS recipe or would you like a NEW one?");
-        String decided = scanner.next();
-        if(decided.equals("THIS")) {
-            return;
-        }if(decided.equals("NEW")){
-            pullDescription();
-        }
+//        String decided = scanner.next();
+//        if(decided.equals("THIS")) {
+//            return;
+//        }if(decided.equals("NEW")){
+//            pullDescription();
+//        }
 
     }
 }
